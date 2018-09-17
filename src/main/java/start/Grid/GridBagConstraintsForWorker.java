da0ba854823953c055fa799d5e0dc631748d0782
timofeeevM <43346197+timofeeevM@users.noreply.github.com>
2018-09-17 22:54:26 +0500

    Create GridBagConstraintsForWorker.java

---
+++ src/main/java/start/Grid/GridBagConstraintsForWorker.java
@@ -0,0 +1,19 @@
+	package Grid;
+	
+	import java.awt.*;
+	
+	public class GridBagConstraintsForWorker extends GridBagConstraints {
+	public GridBagConstraintsForWorker(){
+	anchor = GridBagConstraints.CENTER;
+	fill = GridBagConstraints.NONE;
+	gridheight = 1;
+	gridwidth = 1;
+	gridx = 0;
+	gridy = 1;
+	insets = new Insets(0, 0, 0, 60);
+	ipadx = 0;
+	ipady = 0;
+	weightx = 0.0;
+	weighty = 0.0;
+	}
+	}
