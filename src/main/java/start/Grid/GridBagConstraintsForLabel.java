6892566f4199dbc847914519aeae927c592cd311
timofeeevM <43346197+timofeeevM@users.noreply.github.com>
2018-09-17 22:50:18 +0500

    Create GridBagConstraintsForLabel.java

---
+++ src/main/java/start/Grid/GridBagConstraintsForLabel.java
@@ -0,0 +1,19 @@
+	package Grid;
+	
+	import java.awt.*;
+	
+	public class GridBagConstraintsForLabel extends GridBagConstraints {
+	public GridBagConstraintsForLabel(){
+	anchor = GridBagConstraints.CENTER;
+	fill = GridBagConstraints.HORIZONTAL;
+	gridheight = 1;
+	gridwidth = 3;
+	gridx = 0;
+	gridy = 0;
+	insets = new Insets(0, 0, 30, 0);
+	ipadx = 0;
+	ipady = 0;
+	weightx = 0.0;
+	weighty = 0.0;
+	}
+	}
