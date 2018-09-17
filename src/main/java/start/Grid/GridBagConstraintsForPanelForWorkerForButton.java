46d7f516cc1f72acd06e525f1f6e553175f390a1
timofeeevM <43346197+timofeeevM@users.noreply.github.com>
2018-09-17 22:51:13 +0500

    Create GridBagConstraintsForPanelForWorkerForButton.java

---
+++ src/main/java/start/Grid/GridBagConstraintsForPanelForWorkerForButton.java
@@ -0,0 +1,19 @@
+	package Grid;
+	
+	import java.awt.*;
+	
+	public class GridBagConstraintsForPanelForWorkerForButton extends GridBagConstraints {
+	public GridBagConstraintsForPanelForWorkerForButton(){
+	anchor = GridBagConstraints.CENTER;
+	fill = GridBagConstraints.NONE;
+	gridheight = 1;
+	gridwidth = 1;
+	gridx = 0;
+	gridy = 9;
+	insets = new Insets(50, 50, 50, 50);
+	ipadx = 0;
+	ipady = 0;
+	weightx = 0.0;
+	weighty = 0.0;
+	}
+	}
